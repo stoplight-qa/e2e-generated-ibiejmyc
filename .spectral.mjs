57e436ca0293bb929018f294d92f2f97a89720b2
qa-rtpth-1-vece@example.com <qa-rtpth-1-vece@example.com>
2023-04-24 18:30:15 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16114.stoplight-dev.com/api/v1/projects/cHJqOjcwNg/spectral.js?branch_slug=main&token=d8efcf80-da7d-44ad-aef2-63bd2178a86e";
+export default { extends: ruleset };
